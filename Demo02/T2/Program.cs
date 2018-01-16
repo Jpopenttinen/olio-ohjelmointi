c13e8447f1310b484f0db1a078b68f159f6a47e9
K2593 <K2593@student.jamk.fi>
2018-01-16 15:34:34 +0200

    T2

---
+++ Demo02/T2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] luku = new string[5];
+
+            for (int i = 0; i < luku.Length; i++)
+            {
+                Console.WriteLine("Give a number: ");
+                luku[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine(luku[0], luku[1], luku[2], luku[3], luku[4]);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
