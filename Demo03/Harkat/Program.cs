14fb95b3b8aa4ada778c35c2d3ed31d9e2346af7
K2593 <K2593@student.jamk.fi>
2018-01-18 18:28:46 +0200

    Harkat

---
+++ Demo03/Harkat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harkat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car datsun = new Car();
+            
+
+            datsun.Speed = 100;
+            Console.WriteLine(("datsun spees is: ") + datsun.Speed);
+
+            datsun.Speed = 500;
+            Console.WriteLine(("datsun spees is: ") + datsun.Speed);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo03/Harkat/car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harkat
+{
+    class Car
+    {
+        // max speed
+        private readonly int MaxSpeed = 200;
+        // field
+        public int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value <= MaxSpeed) speed = value;
+                else speed = MaxSpeed;
+            }
+        }
+    }
+}
