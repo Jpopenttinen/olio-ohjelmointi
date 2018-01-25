db9753439c088476e543a0704a9fa02028b1cffd
Joel Penttinen <k2593@student.jamk.fi>
2018-01-25 15:12:38 +0200

    T1

---
+++ Demo03/T1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tank bigOne = new Tank();
+            bigOne.Name = "Tank 12345-3234-UI";
+            bigOne.Type = "154-GU-342";
+
+            int attendedCrew = 0;
+            Console.WriteLine("How many of you lazyasses wanted to attend: ");
+            Int32.TryParse(Console.ReadLine(), out attendedCrew);
+
+            bigOne.CrewCount = attendedCrew;
+
+            if (bigOne.CrewCount == 1)
+                {
+                Console.WriteLine("Give a proper number");
+                }
+            else
+                {
+                Console.WriteLine(("CrewCount is: ") + bigOne.CrewCount);
+                }
+            Console.WriteLine(bigOne.Name);
+
+            float speedAtFirst = bigOne.Speed;
+            Console.WriteLine(("Speed at first is: ") + speedAtFirst);
+
+            int wantedSpeed = 0;
+            Console.WriteLine("Give a speed: ");
+            Int32.TryParse(Console.ReadLine(), out wantedSpeed);
+
+            Console.WriteLine(wantedSpeed);
+            float currentSpeed = bigOne.AccelerateTo(wantedSpeed);
+            Console.WriteLine(currentSpeed);
+            Console.WriteLine(("Big1 speed is: ") + currentSpeed);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demo03/T1/Tank.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T1
+{
+    class Tank
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        private int crewcount;
+        public int CrewCount
+        {
+            get { return crewcount; }
+
+            set
+            {
+                if (value > 2 && value < 6)
+                {
+                    crewcount = value;
+                }
+                else
+                {
+                    crewcount = 1;
+                }
+            }
+        }
+        private float speed = 0;
+        public float Speed
+        {
+            get { return speed; }
+
+            set { }
+        }
+        public readonly float SpeedMax = 100;
+
+        public float AccelerateTo(int accel)
+        {
+
+            for (int i = 0; i < accel; i++)
+            {
+                speed += 5;
+                Console.WriteLine(speed);
+
+                if (speed > 100)
+                {
+                    speed = 100;
+                    break;
+                }
+                
+            }
+            
+
+            return speed;
+        }
+
+        public float SlowTo(int bre)
+        {
+            for (int i = 0; i < bre; i++)
+            {
+                Speed -= 5;
+                Console.WriteLine(Speed);
+
+                if (Speed > 100)
+                {
+                    Speed = 100;
+                    break;
+                }
+            }
+
+            return Speed;
+        }
+
+        private string model;
+        
+        public string Color { get; set; }
+
+
+        private string[] strings = new string[100];
+        public string this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < 100)
+                {
+                    return strings[i];
+                }
+                else
+                {
+
+                    return "out of bounds";
+                }
+
+            }
+            set
+            {
+                if (i >= 0 && i < 100)
+                {
+                    strings[i] = value;
+                }
+
+            }
+        }
+
+
+        public string Model
+        {
+            get { return model; }
+            set
+            {
+                if (value != "toyota" && value != "nissan")
+                {
+                    model = " there is no other than toyota or nissan";
+                }
+                else
+                {
+                    model = value;
+                }
+            }
+        }
+
+      
+    }
+}
