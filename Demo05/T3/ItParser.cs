538beadfcd4416ac8ec22c9498dc1d4deccca152
Joel Penttinen <k2593@student.jamk.fi>
2018-01-29 10:36:05 +0200

    Finish T3

---
+++ Demo05/T3/ItParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3
+{
+    class ItParser
+    {
+        
+        public static int Count(List<int> countNumbers)
+        {
+
+            List<int> countNumb = new List<int>();
+            countNumb = countNumbers;
+
+            int l = countNumb.Count;
+
+
+            return l;
+        }
+
+        public static int Sum(List<int> sumNumbers)
+        {
+            List<int> sumNumb = new List<int>();
+            sumNumb = sumNumbers;
+
+            int k = sumNumb.Sum();
+
+            return k;
+        }
+
+
+        public static int Average(List<int> aveNumbers)
+        {
+            List<int> aveNumb = new List<int>();
+            aveNumb = aveNumbers;
+
+            int x = aveNumb.Sum();
+            int y = aveNumb.Count();
+
+            int z = x / y;
+
+            return z;
+        }
+
+    }
+}
---
+++ Demo05/T3/TestParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3
+{
+    class TestParser
+    {
+        static void Main()
+        {
+            
+            //int[] numArray = new int[i];
+
+            List<int> numArray = new List<int>();
+            Console.WriteLine("Give an array of numbers separated by a collom: ");
+
+            string userInput = Console.ReadLine();
+            string[] inputArray = userInput.Split(',');
+            int nums;
+            foreach (string j in inputArray)
+            {
+                if(Int32.TryParse(j, out nums))
+                {
+                    numArray.Add(nums);
+                    
+                }
+            }
+
+            int arrayCount = ItParser.Count(numArray);
+            int arraySum = ItParser.Sum(numArray);
+            int arrayAverage = ItParser.Average(numArray);
+
+
+            Console.WriteLine("Given array has " + arrayCount + " numbers which sum is " + arraySum + " and an average number is " + arrayAverage);
+
+
+            //numArray.ForEach(Console.WriteLine);
+
+            
+
+
+            Console.ReadLine();
+
+            /*for (; i < numArray.Length; i++)
+            {
+                numArray[i];
+            }*/
+
+        }
+    }
+}
