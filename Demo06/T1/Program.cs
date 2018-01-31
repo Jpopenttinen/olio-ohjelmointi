e5eaf94de35d24fbf845da55718d982a052d710d
K2593 <K2593@student.jamk.fi>
2018-01-31 12:46:51 +0200

    Finishing T1

--- Demo06/T1/Program.cs
+++ Demo06/T1/Program.cs
@@ -18,10 +18,33 @@ namespace T1
 
             Boss boss_001 = new Boss("Janne", "Ohjelmoija", 5200, "Nissa", 200);
 
-            
-            Console.WriteLine(emp_001.ToString());
+            List<Employee> lstdEmployees = new List<Employee>();
 
-            
+            lstdEmployees.Add(emp_001);
+            lstdEmployees.Add(emp_002);
+            lstdEmployees.Add(emp_003);
+            lstdEmployees.Add(emp_004);
+
+            Console.WriteLine("Employees:");
+            Console.WriteLine("");
+
+            foreach (Employee k in lstdEmployees)
+            {
+                Console.WriteLine(k.ToString());
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Bosses:");
+            Console.WriteLine("");
+
+            List<Boss> lstdBosses = new List<Boss>();
+            lstdBosses.Add(boss_001);
+
+            foreach (Boss n in lstdBosses)
+            {
+                Console.WriteLine(n.ToString());
+            }
+
             Console.ReadLine();
         }
     }
