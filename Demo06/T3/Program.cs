5c4e2c52da03ec376d7eac102e03aad0b5a56f87
Joel Penttinen <k2593@student.jamk.fi>
2018-02-01 09:08:22 +0200

    T3

--- Demo06/T3/Program.cs
+++ Demo06/T3/Program.cs
@@ -9,6 +9,43 @@ namespace T3
     {
         static void Main(string[] args)
         {
+
+
+            Radio Bose = new Radio();
+            int ONoff = 1;
+            string freqString;
+            float freq;
+            int volume;
+            Bose.Volume = 1;
+            while (true)
+            {
+                Console.WriteLine("If you wish to put Radio on, give 1 (if you wish to shut program give 0): ");
+                Int32.TryParse(Console.ReadLine(), out ONoff);
+
+                if (ONoff == 1) { Bose.OnOff = true; }
+                else { Bose.OnOff = false; }
+
+                Console.WriteLine("Set the frequency: ");
+                freqString = Console.ReadLine();
+                //float.Parse("41.00027357629127", CultureInfo.InvariantCulture.NumberFormat
+                //freq = (float)Convert.ToDouble(freqString);
+                freq = float.Parse(freqString);
+                Bose.Frequency = freq;
+
+                Console.WriteLine("Set the volume: ");
+                Int32.TryParse(Console.ReadLine(), out volume);
+                Bose.Volume = volume; 
+
+                Console.WriteLine(Bose.OnOff);
+                Console.WriteLine(Bose.Frequency);
+                Console.WriteLine(Bose.Volume);
+
+                Console.ReadLine();
+            }
+
+                
+
+
         }
     }
 }
--- Demo06/T3/Radio.cs
+++ Demo06/T3/Radio.cs
@@ -10,9 +10,39 @@ namespace T3
         // päälle/pois-kytkin, äänen voimakkuuden säädin (arvot 0, 1, 2,..., 9) ja 
         // kuunneltavan kanavan taajuusvalinta (2000.0 - 26000.0)
 
-        public bool OnOff { get; set; }
-        public int Volume { get; set; }
-        public float Frequency { get; set; }
+
+        private bool onOff;
+        public bool OnOff
+        {
+            get { return onOff; }
+
+            set {
+
+                if (value == true)
+                    {
+                    onOff = true;
+                    }
+                else { onOff = false; }
+
+                }
+        }
+
+        private int volume;
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value <= 0 && value < 10) { volume = value; }
+                else { Console.WriteLine("Given value isn't in a volume range"); }
+            }
+        }
+        private float frequency;
+        public float Frequency
+        {
+            get { return frequency; }
+            set { if (value <= 2000.0 && value <= 26000.0) { frequency = value; } }
+        }
 
 
 
