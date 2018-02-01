333fde8885b5786b3ab1d75fb651a8bbae3a74d7
Joel Penttinen <k2593@student.jamk.fi>
2018-02-01 07:56:15 +0200

    Start of T3

---
+++ Demo06/T3/Radio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3
+{
+    class Radio
+    {
+        // päälle/pois-kytkin, äänen voimakkuuden säädin (arvot 0, 1, 2,..., 9) ja 
+        // kuunneltavan kanavan taajuusvalinta (2000.0 - 26000.0)
+
+        public bool OnOff { get; set; }
+        public int Volume { get; set; }
+        public float Frequency { get; set; }
+
+
+
+
+    }
+}
