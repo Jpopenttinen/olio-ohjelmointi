3e6a7053a7a56d531c352a86ae3eb2d7de8a7f38
Joel Penttinen <k2593@student.jamk.fi>
2018-02-01 12:52:26 +0200

    T4

---
+++ Demo06/T4/Media.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4
+{
+    class Media
+    {
+
+        // Media --> Tekijä, vuosi, tyyppi, nimi
+        // Kirja : Media --> kustantaja, sivunro
+        // Digi : Media --> tuottaja, nimi, kokonaispituus
+
+        // Elektronik --> Valmistaja, Malli, valmistusvuosi, takuu,
+
+        public string Name;
+        public string Author;
+        public DateTime Date;
+        public string Type;
+
+        public Media(string name, string author, DateTime date, string type)
+        {
+            Name = name;
+            Author = author;
+            Date = date;
+            Type = type;
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Profession: " + Author + ", Salary: " + Date + ", Type: " + Type;
+        }
+
+
+
+    }
+}
