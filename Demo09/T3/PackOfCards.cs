df54e8269716d3efb32cae0c483106d7e7325b76
Joel Penttinen <k2593@student.jamk.fi>
2018-02-12 09:58:31 +0200

    T3 and T4

---
+++ Demo09/T3/PackOfCards.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3
+{
+    static class CardShuffle
+    {
+
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            
+        }
+
+        public static List<string> Fisher_Yates_CardDeck_Shuffle(List<string> aList)
+        {
+
+            System.Random _random = new System.Random();
+
+            string myGO;
+
+            int n = aList.Count;
+            for (int i = 0; i < n; i++)
+            {
+                // NextDouble returns a random number between 0 and 1.
+                // ... It is equivalent to Math.random() in Java.
+                int r = i + (int)(_random.NextDouble() * (n - i));
+                myGO = aList[r];
+                aList[r] = aList[i];
+                aList[i] = myGO;
+            }
+
+            return aList;
+        }
+
+
+
+    }
+
+}
---
+++ Demo09/T3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<string> Cards = new List<string>();
+            /*List<string> Diamonds = new List<string>();
+            List<string> Hearts = new List<string>();
+            List<string> Spades = new List<string>();*/
+
+            //Clubs
+            string club = "Club#";
+            for (int i = 1; i < 14; i++)
+            {
+                Cards.Add(string.Concat(club, i));
+            }
+            //Diamonds
+            string diamond = "Diamond#";
+            for (int i = 1; i < 14; i++)
+            {
+                Cards.Add(string.Concat(diamond, i));
+            }
+            //Hearts
+            string heart = "Heart#";
+            for (int i = 1; i < 14; i++)
+            {
+                Cards.Add(string.Concat(heart, i));
+            }
+            //Spades
+            string spade = "Spade#";
+            for (int i = 1; i < 14; i++)
+            {
+                Cards.Add(string.Concat(spade, i));
+            }
+
+            int n = 0;
+            foreach (string b in Cards)
+            {
+                n++;
+                Console.WriteLine(n + " card is " + b);  // TEHTÄVÄ 3
+            }
+
+
+            Console.WriteLine(" ");
+            Console.WriteLine("TEHTÄVÄ 4!!!!");
+            Console.WriteLine(" ");
+
+            List<string> Shuffled = new List<string>();
+
+            Shuffled = CardShuffle.Fisher_Yates_CardDeck_Shuffle(Cards);
+            int k = 0;
+            foreach (string b in Shuffled)
+            {
+                k++;
+                Console.WriteLine(k + " card is " + b);  // TEHTÄVÄ 4
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
