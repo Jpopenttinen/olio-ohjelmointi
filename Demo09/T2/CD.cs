abaa57615db2c3caee59a5594a5f5974326b879b
Joel Penttinen <k2593@student.jamk.fi>
2018-02-12 07:53:11 +0200

    T2

---
+++ Demo09/T2/CD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T2
+{
+    class CD
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public List<string> Song { get; set; }
+
+
+        public CD(string name, string artist, List<string> song)
+        {
+            Name = name;
+            Artist = artist;
+            Song = song;
+        }
+
+        public override string ToString()
+        {
+            //var ListSongs = new List<string>();
+
+            foreach (string a in Song)
+            {
+                return a;
+            }
+            return Name + Artist;
+        }
+
+
+    }
+}
---
+++ Demo09/T2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string song1 = "Toot";
+            string song2 = "truut";
+            string song3 = "pruut";
+            string song4 = "tuut";
+            string song5 = "Jee";
+            string song6 = "Jippii";
+
+            List<string> Sounds = new List<string>();
+
+            Sounds.Add(song1);
+            Sounds.Add(song2);
+            Sounds.Add(song3);
+            Sounds.Add(song4);
+            Sounds.Add(song5);
+            Sounds.Add(song6);
+
+
+            CD cd = new CD
+            (
+            "Holidays",
+            "best singer",
+            Sounds
+            );
+
+            int k = 0;
+
+            Console.WriteLine("CD Data: ");
+            Console.WriteLine("CD Name: " + cd.Name);
+            Console.WriteLine("Artist: " + cd.Artist);
+            Console.WriteLine("Songs: ");
+            foreach(string a in cd.Song)
+            {
+                k++;
+                Console.WriteLine(k + ". " + a);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
