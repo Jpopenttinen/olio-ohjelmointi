2a56e5fa11f45cd57cdbc5b04539c18defcc8dad
K2593 <K2593@student.jamk.fi>
2018-02-19 16:17:33 +0200

    T2

--- Demo03/T2/Elevator.cs
+++ Demo03/T2/Elevator.cs
@@ -7,15 +7,42 @@ namespace T2
 {
     class Elevator
     {
-        public int ElevStatus { get; set; }
-
-        public void GetUp()
+        private int elevStatus;
+        public int ElevStatus
         {
-
+            get
+            {
+                return elevStatus;
+            }
+            set
+            {
+                if (value > 1 || value < 5)
+                {
+                    elevStatus = value;
+                }
+                else { Console.WriteLine("There is not such a level in this building"); }
+            }
+                
         }
-        public void GetDown()
+        public int GetUp(int level, int atBegin)
         {
 
+
+            for (int i = atBegin; i < level; i++)
+            {
+                ElevStatus++;
+                Console.WriteLine(ElevStatus);
+            }
+            return elevStatus;
+        }
+        public int GetDown(int level, int atBegin)
+        {
+            for (int i = atBegin; i > level; i--)
+            {
+                ElevStatus--;
+                Console.WriteLine(ElevStatus);
+            }
+            return elevStatus;
         }
 
     }
--- Demo03/T2/Program.cs
+++ Demo03/T2/Program.cs
@@ -9,6 +9,31 @@ namespace T2
     {
         static void Main(string[] args)
         {
+            int givenLevel;
+            int levelNow = 0;
+            int status;
+
+            Elevator Kone = new Elevator();
+
+            while (true)
+            {
+                Console.WriteLine("Where you wish to go: (heaven in 6)");
+                int.TryParse(Console.ReadLine(), out givenLevel);
+                if (givenLevel == 6) { Console.WriteLine("You have chosen to leave the prog"); break; }
+
+                if (givenLevel > levelNow)
+                {
+                    status = Kone.GetUp(givenLevel, levelNow);
+                }
+                else
+                {
+                    status = Kone.GetDown(givenLevel, levelNow);
+                }
+                levelNow = status;
+
+                Console.WriteLine("You are now at level: " + status);
+            }
+
         }
     }
 }
